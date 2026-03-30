30a2c9df7c0977481039075aa181b4ca1892076a
DANILO DOS SANTOS SILVA <danilo.ssilva45@senacsp.edu.br>
2026-03-30 20:46:32 -0300

    alteração header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PessoaloginComponent } from './pags/pessoalogin/pessoalogin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component: HomeComponent },
+    { path:"login", component: PessoaloginComponent}
+
+]
